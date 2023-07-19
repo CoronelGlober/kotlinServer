96356c62bbacc3bca5d7e1f3699ed27e36b321ca
David Coronel <david.coronel@autodesk.com>
2023-07-19 16:06:24 -0500

    added /me server path

--- src/main/kotlin/cats/com/plugins/Routing.kt
+++ src/main/kotlin/cats/com/plugins/Routing.kt
@@ -5,6 +5,7 @@ import io.ktor.server.response.*
 import io.ktor.server.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.swagger.*
+import io.ktor.util.*
 
 fun Application.configureRouting() {
     
@@ -12,6 +13,10 @@ fun Application.configureRouting() {
         swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/me") {
+            val name = call.attributes.getOrNull(AttributeKey("name")) ?: "dear unknown"
+            call.respondText("Hello $name")
         }
         // Static plugin. Try to access `/static/index.html`
         static("/static") {
